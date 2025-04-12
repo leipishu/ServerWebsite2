361f3f98adafc8c0b5397b48fcfe03eadaf38c3d
leipishu <kimi.peng@outlook.com>
2025-04-12 20:34:50 +0800

    refactor: 移除base标签和不必要的脚本引用

--- js/links.js
+++
@@ -1,8 +0,0 @@
-// js/links.js
-function jumpToNoticeLink() {
-    window.location.href = "https://mc.leipishu.top/pages/notices";
-}
-
-function jumpToHomeLink() {
-    window.location.href = "https://mc.leipishu.top/";
-}
