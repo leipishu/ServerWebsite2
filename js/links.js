f025b4654b65eaf8895d27543bb0987051ccb696
leipishu <kimi.peng@outlook.com>
2025-04-13 18:14:05 +0800

    feat: 新增跳转公告和主页的JavaScript函数

---
+++ js/links.js
@@ -0,0 +1,16 @@
+function getHomePageUrl() {
+    // 检查当前是否在 Vercel 上
+    if (window.location.hostname === 'mc.leipishu.top') {
+        return '/';
+    } else {
+        return 'index.html';
+    }
+}
+
+function goToHomePage() {
+    window.location.href=getHomePageUrl()
+}
+
+function goToNotices(){
+    window.location.href='notices.html';
+}
